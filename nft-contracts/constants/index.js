6c2559e31b5e1a147c70fd3ea179f708b8752740
omelbourne <43779654+omelbourne@users.noreply.github.com>
2022-03-19 23:33:40 +0000

    Create mint app

--- nft-contracts/constants/index.js
+++ nft-contracts/constants/index.js
@@ -1,6 +1,6 @@
 // Address of whitelist contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xAe8427360eC1dCaAE33562D1c790DB2e56E74dAA";
-// URL to extract Metadata for Antiparalle NFTs
+// URL to extract Metadata for Antiparallel NFTs
 const METADATA_URL = "";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
