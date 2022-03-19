9f64b2e138550d113be2625fb2304e068020c42e
omelbourne <43779654+omelbourne@users.noreply.github.com>
2022-03-19 23:48:14 +0000

    Remove unescaped entity

--- nft-app/pages/index.js
+++ nft-app/pages/index.js
@@ -316,7 +316,7 @@ export default function Home() {
     if (!presaleStarted) {
       return (
         <div>
-          <div className={styles.description}>Presale hasn't started!</div>
+          <div className={styles.description}>Presale has not started!</div>
         </div>
       );
     }
